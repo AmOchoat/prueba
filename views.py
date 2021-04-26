4ff1693c1ff5f7e300eee986394027d8ae606d75
Ubuntu <ubuntu@ip-172-31-29-236.ec2.internal>
2021-04-26 01:11:18 +0000

    v.py

---
+++ views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+from .forms import MeasurementForm
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .logic.logic_measurement import create_measurement, get_measurements
+from django.contrib.auth.decorators import login_required
+from monitoring.auth0backend import getRole
+
+@login_required
+def measurement_list(request):
+    role = getRole(request)
+    if role == "Supervisor" or role == "Gerencia Campus":
+        measurements = get_measurements()
+        context = {
+            'measurement_list': measurements
+        }
+        return render(request, 'Measurement/measurements.html', context)
+    else:
+        return HttpResponse("Aqui mandan las divinas, no puede pasar")
+
+def measurement_create(request):
+    if request.method == 'POST':
+        form = MeasurementForm(request.POST)
+        if form.is_valid():
+            create_measurement(form)
+            messages.add_message(request, messages.SUCCESS, 'Measurement create successful')
+            return HttpResponseRedirect(reverse('measurementCreate'))
+        else:
+            print(form.errors)
+    else:
+        form = MeasurementForm()
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'Measurement/measurementCreate.html', context)
